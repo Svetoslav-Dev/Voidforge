2c07e75b21e0a2ee311ab7c14e87e7c6a1ffb865
FilthySmugCookie <svetoslav.dichkov@gmail.com>
2026-08-02 19:18:18 +0300

    Make test database settings environment aware

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -4,10 +4,22 @@ namespace Tests;
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+use Illuminate\Routing\Middleware\ThrottleRequestsWithRedis;
 use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware([
+            ThrottleRequests::class,
+            ThrottleRequestsWithRedis::class,
+        ]);
+    }
+
     /**
      * Create the application for tests with an isolated MariaDB database.
      */
@@ -16,12 +28,12 @@ abstract class TestCase extends BaseTestCase
         /** @var Application $app */
         $app = parent::createApplication();
 
-        $app['config']->set('database.default', 'mariadb');
-        $app['config']->set('database.connections.mariadb.host', 'db');
-        $app['config']->set('database.connections.mariadb.port', '3306');
-        $app['config']->set('database.connections.mariadb.database', 'voidforge_test');
-        $app['config']->set('database.connections.mariadb.username', 'voidforge');
-        $app['config']->set('database.connections.mariadb.password', 'secret');
+        $app['config']->set('database.default', env('DB_CONNECTION', 'mariadb'));
+        $app['config']->set('database.connections.mariadb.host', env('DB_HOST', '127.0.0.1'));
+        $app['config']->set('database.connections.mariadb.port', env('DB_PORT', '3306'));
+        $app['config']->set('database.connections.mariadb.database', env('DB_DATABASE', 'voidforge_test'));
+        $app['config']->set('database.connections.mariadb.username', env('DB_USERNAME', 'voidforge'));
+        $app['config']->set('database.connections.mariadb.password', env('DB_PASSWORD', 'secret'));
         $app['config']->set('session.driver', 'array');
         $app['config']->set('cache.default', 'array');
         $app['config']->set('queue.default', 'sync');
